9148dcead95b63c3d55028f87b0a6f4cc24b7491
jaken92 <jaaakeeen@gmail.com>
2025-01-24 16:49:24 +0100

    disable gb routes

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_URL);
-const db = mongoose.connection;
+// mongoose.connect(process.env.DB_URL);
+// const db = mongoose.connection;
 
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to db"));
-// db.on("connected", () => console.log("Connected to database"));
+// db.on("error", (error) => console.log(error));
+// db.once("open", () => console.log("Connected to db"));
 
 app.use(express.json());
 
-const guestbookRouter = require("./routes/guestbookRouter");
+// const guestbookRouter = require("./routes/guestbookRouter");
 const mailRouter = require("./routes/mailRouter");
 
-app.use("/guestbook", guestbookRouter);
+// app.use("/guestbook", guestbookRouter);
 app.use("/mail", mailRouter);
 
 app.listen(3000, () => console.log("Server started"));
