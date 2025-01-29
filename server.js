d5729785cd191aab0d3cf37a610cdb28b16a0dd8
jaken92 <jaaakeeen@gmail.com>
2025-01-29 13:07:35 +0100

    update cors

--- server.js
+++ server.js
@@ -12,13 +12,23 @@ const app = express();
 // db.on("error", (error) => console.log(error));
 // db.once("open", () => console.log("Connected to db"));
 
-const corsOptions = {
-  origin: "https://petterjakobsson.netlify.app/",
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "https://petterjakobsson.netlify.app/",
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+// Use FRONTEND_URL if set, otherwise default to localhost
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+
+
+// Set CORS headers to allow requests from the frontend
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
+  next();
+});
 
 app.use(express.json());
 
